0981bbda670268f4e57c30f617f41b690e3375e3
Cynthia <cynthia@coan.dev>
2020-05-04 16:58:53 -0600

    whoops need to own the directory too

--- .dl/helpers/rust/ensure-toolchain.sh
+++ .dl/helpers/rust/ensure-toolchain.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ensureRustToolchainConfig() {
+  sudo -n chown "$(whoami)" -R ~/
   mkdir -p $HOME/.cargo
-  sudo -n chown "$(whoami)" -R ~/.cargo/
   echo "[build]
 # Target musl-libc by default when running Cargo.
 target = \"x86_64-unknown-linux-musl\"
